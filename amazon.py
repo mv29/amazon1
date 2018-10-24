e9411abaa4cdcb52e7880125ceba4d5d55bf71d9
mv29 <mrinal29verma@gmail.com>
2018-10-24 18:52:46 +0530

    comment fetching of a product done and file made more readable

--- amazon.py
+++ amazon.py
@@ -2,6 +2,7 @@ import json
 from time import sleep
 from  lxml import html
 import requests
+import csv
 
 def lol(a):
     str=None
@@ -128,47 +129,233 @@ def amaz3(url,j):
     return [url,j]
 
 
+
+#getting the links for the comments according to their rating for further traversing
+def amaz4(url):
+
+    list_comments_url=[]
+    print("page under processing",url)
+    page = requests.get(url, headers={'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.149 Safari/537.36'})
+    if page:
+        print("page loaded")
+    else:
+        print("error in page load")
+
+    doc = html.fromstring(page.content)
+    if len(doc)!=0:
+        print("html parsed")
+    else :
+        print("error in html parsing")
+
+    xpath_5 = '//table[@id="histogramTable"]//a[@title="5 star"]/@href'
+    raw_5 = doc.xpath(xpath_5)
+    #print(raw_5)
+    list_comments_url.append(raw_5[0])
+
+    xpath_4 = '//table[@id="histogramTable"]//a[@title="4 star"]/@href'
+    raw_4 = doc.xpath(xpath_4)
+    #print(raw_4)
+    list_comments_url.append(raw_4[0])
+
+    xpath_3 = '//table[@id="histogramTable"]//a[@title="3 star"]/@href'
+    raw_3 = doc.xpath(xpath_3)
+    #print(raw_3)
+    list_comments_url.append(raw_3[0])
+
+    xpath_2 = '//table[@id="histogramTable"]//a[@title="2 star"]/@href'
+    raw_2 = doc.xpath(xpath_2)
+    #print(raw_2)
+    list_comments_url.append(raw_2[0])
+
+    xpath_1 = '//table[@id="histogramTable"]//a[@title="1 star"]/@href'
+    raw_1 = doc.xpath(xpath_1)
+    #print(raw_1)
+    list_comments_url.append(raw_1[0])
+
+    return list_comments_url
+
+
+#class for storing the comments and their necessary information
+class comments():
+
+    count=0
+    def __init__(self, rating,title,name,comment,helpful):
+        self.rating =rating
+        self.title = title
+        self.name=name
+        self.comment=comment
+        self.helpful=helpful
+        comments.count += 1
+
+COMMENTS=[]
+
+
+# traversing the link of product comment page and traversing to the next page and storing the reqiured information in a list which contains the comment as a object
+def amaz5(url,rating):
+    print("page under processing", url)
+
+    page = requests.get(url, headers={
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.149 Safari/537.36'})
+
+    if page:
+        print("page loaded")
+    else:
+        print("error in page load")
+
+    doc = html.fromstring(page.content)
+
+    if len(doc) != 0:
+        print("html parsed")
+    else:
+        print("error in html parsing")
+
+    for i in range(2,12):
+
+        xpath_title = "//div[@id=\"cm_cr-review_list\"]/div[" + str(i)+ "]//div[@class=\"a-row\"]//a[@data-hook=\"review-title\"]/text()"
+        raw_title = doc.xpath(xpath_title)
+        if len(raw_title)!=0:
+            print(raw_title[0])
+        else:
+            raw_title.append(None)
+
+        xpath_author = "//div[@id=\"cm_cr-review_list\"]/div[" + str(i) + "]//div[@class=\"a-row\"]//a[@data-hook=\"review-author\"]/text()"
+        raw_author = doc.xpath(xpath_author)
+        if len(raw_author) != 0:
+            print(raw_author[0])
+        else:
+            raw_author.append(None)
+
+        xpath_comment = "//div[@id=\"cm_cr-review_list\"]/div[" + str(i) + "]//div[contains(@class, 'a-row a-spacing-small review-data')]//span[@data-hook=\"review-body\"]/text()"
+        raw_comment = doc.xpath(xpath_comment)
+        if len(raw_comment) != 0:
+            print(raw_comment[0])
+        else:
+            raw_comment.append(None)
+
+        xpath_help = "//div[@id=\"cm_cr-review_list\"]/div[" + str(i) + "]//div[contains(@class, 'a-row a-expander-container a-expander-inline-container')]//span[@data-hook=\"review-voting-widget\"]//div//span[@data-hook=\"helpful-vote-statement\"]/text()"
+        raw_help = doc.xpath(xpath_help)
+        helper=[]
+        if len(raw_help) != 0:
+            helper=raw_help[0].split(' ')
+            print(helper[0])
+        else:
+            helper.append(None)
+
+        COMMENTS.append(comments(rating,raw_title[0],raw_author[0],raw_comment[0],helper[0]))
+        print("\n")
+
+    xpath_nextpage = "//div[@id=\"cm_cr-pagination_bar\"]//li[@class=\"a-last\"]/a/@href"
+    raw_nextpage = doc.xpath(xpath_nextpage)
+    print("\n")
+    return raw_nextpage
+
+
+
 url1="https://www.amazon.in/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=phone"
 
 
 
-"""
-----for loop for amaz1 function
-for i in range(1):
-    if i==0:
-        j=int(0)
-        lol1=amaz1(url1,j)
-        sleep(10)
-        url=lol1[0]
-        j = lol1[1]
-    print(url,j,)
-    url,j = amaz1(url,j)
+#calling function for amaz1 function
+# gathering the asin no of all products in a page and traversing to next page
+def mv3():
+    for i in range(1):
+        if i == 0:
+            j = int(0)
+            lol1 = amaz1(url1, j)
+            sleep(10)
+            url = lol1[0]
+            j = lol1[1]
+        print(url, j, )
+        url, j = amaz1(url, j)
 
 
--- for loop for amaz2 function
-extracted_data=[]
-for i in ANSI:
-    print(i)
-    url='https://www.amazon.in/dp/'+i[0]
-    print('processing -',url)
-    extracted_data.append(amaz2(url))
-    sleep(15)
-"""
+
+
+
+
+
+# fetching information of a product from a products page and and traversing to the other products in the list
+#for loop for amaz2 function
+#ANSI list will be generated from mv3
+def mv4():
+    extracted_data = []
+    for i in ANSI:
+        print(i)
+        url = 'https://www.amazon.in/dp/' + i[0]
+        print('processing -', url)
+        extracted_data.append(amaz2(url))
+        sleep(15)
+
+
+
+
+
+
 
 # fro loop for amaz3 function
-for i in range(6):
-    print("page-",i, "processing")
-    if i==0:
-        j=int(0)
-        lol1=amaz3(url1,j)
-        sleep(10)
-        url=lol1[0]
-        j = lol1[1]
-        continue
-    url,j = amaz3(url,j)
+# for loop for fetching information of all products in a page and then moving on two next page for further information
+def mv1():
+    for i in range(6):
+        print("page-", i, "processing")
+        if i == 0:
+            j = int(0)
+            lol1 = amaz3(url1, j)
+            sleep(10)
+            url = lol1[0]
+            j = lol1[1]
+            continue
+        url, j = amaz3(url, j)
 
-print(len(products))
+    print(len(products))
 
-with open("data.json", "w") as json_file:
-    json.dump(products, json_file, indent=4)
-    json_file.write("\n")
+    with open("data.json", "w") as json_file:
+        json.dump(products, json_file, indent=4)
+        json_file.write("\n")
+
+
+
+
+
+
+
+
+
+# fetching the all the comments of a product and storing them according to the rating
+# title,name of author,comment,upvotes of a comment stored
+#tested succesfully for 75 pages parsing
+
+def mv2(url):
+    lol= amaz4(url)
+
+    for i in range(len(lol)):
+        print("processing comments with ratng",5-i," " ,lol[i])
+        current_page="https://www.amazon.in"+lol[i]
+        for j in range(10):
+            next_page = amaz5(current_page, 5 - i)
+            sleep(10)
+            current_page = "https://www.amazon.in" + next_page[0]
+            print(current_page)
+
+    print(len(COMMENTS))
+    for i in range(len(COMMENTS)):
+        print(COMMENTS[i].rating)
+        print(COMMENTS[i].title)
+        print(COMMENTS[i].name)
+        print(COMMENTS[i].comment)
+        print(COMMENTS[i].helpful)
+
+    with open('dob.csv', 'w') as f:
+        writer = csv.writer(f)
+        for row in COMMENTS:
+            mv=[]
+            mv.append(row.rating)
+            mv.append(row.title)
+            mv.append(row.name)
+            mv.append(row.comment)
+            mv.append(row.helpful)
+            writer.writerow(mv)
+            mv.clear()
+
+
+
+mv2('https://www.amazon.in/dp/B077PWBC6V')
